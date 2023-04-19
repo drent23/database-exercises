6d73ef3c9b0df5135c1cc75fb7b2f2d07a35e47e
David Renteria <david.j.renteria1@gmail.com>
2023-04-19 14:19:47 -0500

    added records

---
+++ albums_seeder.sql
@@ -0,0 +1,27 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop'),
+       ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard Rock'),
+       ('Eagles', 'Their Greatest Hits', 1976, 41.2, 'Country rock'),
+       ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock'),
+       ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country'),
+       ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard rock'),
+       ('Michael Jackson', 'Bad', 1987, 21.2, 'Pop'),
+       ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Alternative rock'),
+       ('Celine Dion', 'Falling Into You', 1996, 21.1, 'Pop'),
+       ('Adele', '21', 2011, 27.1, 'Pop'),
+       ('The Beatles', '1', 2000, 23.2, 'Rock'),
+       ('Metallica', 'Metallica', 1991, 22.7, 'Heavy metal'),
+       ('Celine Dion', 'Let''s Talk About Love', 1997, 20.5, 'Pop'),
+       ('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22.9, 'Reggae'),
+       ('Guns N'' Roses', 'Appetite for Destruction', 1987, 22.8, 'Hard rock'),
+       ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 22, 'Heartland rock'),
+       ('ABBA', 'Gold: Greatest Hits', 1992, 21.6, 'Pop'),
+       ('Dire Straits', 'Brothers in Arms', 1985, 21.1, 'Roots rock'),
+       ('Santana', 'Supernatural', 1999, 20.8, 'Latin rock'),
+       ('Madonna', 'The Immaculate Collection', 1990, 19.8, 'Pop'),
+       ('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive rock'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.3, 'Rock'),
+       ('Michael Jackson', 'Dangerous', 1991, 17, 'Pop'),
+       ('The Beatles', 'Abbey Road', 1969, 16.9, 'Rock');
+
