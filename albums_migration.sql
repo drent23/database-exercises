ef803b6dccdf22bf9a6e8ca6653c3fc1cd0be6ab
David Renteria <david.j.renteria1@gmail.com>
2023-04-19 10:08:30 -0500

    tables exer done

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
